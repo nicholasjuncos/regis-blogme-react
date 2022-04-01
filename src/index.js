4ad1d30df7c8419db344aca85b9ca619697b87a7
Nicholas Juncos <nicholas.juncos@protonmail.com>
2022-03-31 21:56:58 -0500

    Changed sentry configuration slightly.

--- src/index.js
+++ src/index.js
@@ -25,11 +25,7 @@ import store from "./store";
 
 Sentry.init({
   dsn: process.env.REACT_APP_SENTRY_DSN,
-  integrations: [
-    new Integrations.BrowserTracing({
-      tracingOrigins: ["localhost", /^\//],
-    }),
-  ],
+  integrations: [new Integrations.BrowserTracing()],
 
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
